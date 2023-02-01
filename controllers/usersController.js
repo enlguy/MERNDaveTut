f7645bb35fcf3612aa798c5dd890448222684ace
Jack <jack282323@gmail.com>
2023-02-01 19:04:27 +0100

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,7 +9,7 @@ const bcrypt = require('bcrypt')
 
 const getAllUsers = asyncHandler(async (req, res) => {
     const users = await User.find().select('-password').lean()
-    if (!users) {
+    if (!users?.length) {
         return res.status(400).json({ message: 'No users found'})
     }
     res.json(users)
@@ -35,7 +35,7 @@ const createNewUser = asyncHandler(async (req, res) => {
     }
 
     // Hash password
-    const hashedPass = await bcrypt.hash(password, 10) // salt rounds
+    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
 
     const userObject = { username, "password": hashedPwd, roles }
 
@@ -54,7 +54,38 @@ const createNewUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const updateUser = asyncHandler(async (req, res) => {
-    
+    const { id, username, roles, active, password } = req.body
+
+    // Confirm data
+    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+        return res.status(400).json({ message: 'All fields are required'})
+    }
+
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(400).json({ message: 'User not found'})
+    }
+
+    // Check for duplicate
+    const duplicate = await User.findOne({ username }).lean().exec()
+    // Allow updates to the original user
+    if (duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({ message: 'Duplicate username' })
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+    if (password) {
+        // Hash password
+        user.password = await bcrypt.hash(password, 10) // salt rounds
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({ message: `${updatedUser.username} updated` })
 })
 
 // @desc Delete a user
@@ -62,7 +93,28 @@ const updateUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const deleteUser = asyncHandler(async (req, res) => {
+    const { id } = req.body
+    
+    if (!id) {
+        return res.status(400).json({ message: 'User ID Required' })
+    }
 
+    const note = await Note.findOne({ user: id }).lean().exec()
+    if (note) {
+        return res.status(400).json({ message: 'User has assigned notes' })
+    }
+
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(400).json({ message: 'User not found' })
+    }
+
+    const result = await User.deleteOne()
+
+    const reply = `Username ${result.username} with ID ${result._id} deleted`
+
+    res.json(reply)
 })
 
 module.exports = {

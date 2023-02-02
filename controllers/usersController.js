90d0f846b1960342fcc70dade0d6bc6961daeee6
Jack <jack282323@gmail.com>
2023-02-02 11:22:46 +0100

    Updating from local - notes controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -110,7 +110,7 @@ const deleteUser = asyncHandler(async (req, res) => {
         return res.status(400).json({ message: 'User not found' })
     }
 
-    const result = await User.deleteOne()
+    const result = await user.deleteOne()
 
     const reply = `Username ${result.username} with ID ${result._id} deleted`
 
